80c28f8fce52079d368b104fd5ad831bfc506983
nairi.huang <nairi.huang@admindeMacBook-Air.local>
2024-07-24 18:37:41 +0800

    calc avg len of reviews

--- read.py
+++ read.py
@@ -6,8 +6,11 @@ with open('reviews.txt','r') as f:
 		count += 1
 		if count %10000 ==0 :
 		  print(len(data))
-print(data[0])
-print('-------------')
-print(data[3])
+print('讀取完畢,總共有',len(data),'筆資料')
 
+sum_len =0
+for d in data:
+	sum_len =sum_len +len(d)
+	
+print('每筆平均是',sum_len /len(data))
 
